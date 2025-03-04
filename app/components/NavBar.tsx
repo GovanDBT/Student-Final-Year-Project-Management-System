a154ec0bbd35e27834d52a7bd23b649d77cbbde3
GovanDBT <govan.dibotelo@gmail.com>
2025-03-04 05:04:06 +0200

    remove dark mode

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -15,17 +15,44 @@ const NavBar = () => {
     { label: "Project Milestone", href: "/milestone" },
   ];
   return (
-    <nav className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <FaBarsStaggered size={23} />
+    <nav className=" navbar bg-base-100 shadow-sm">
+      <div className="container mx-auto flex">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered size={23} />
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-lg dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                {links.map((link) => (
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className={classnames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-lg dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
+          <Link href="/" className="btn btn-ghost text-xl space-x-2">
+            <Image src="/UBotswana.png" alt="UB logo" width={30} height={30} />
+            <h1 className="font-bungee text-2xl hidden md:flex">
+              Project Manager
+            </h1>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu">
+            <li className="menu-horizontal space-x-10">
               {links.map((link) => (
                 <Link
                   key={link.href}
@@ -42,43 +69,11 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-        <Link href="/" className="btn btn-ghost text-xl space-x-2">
-          <Image src="/UBotswana.png" alt="UB logo" width={30} height={30} />
-          <h1 className="font-bungee text-2xl">Project Manager</h1>
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu">
-          <li className="menu-horizontal space-x-15">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={classnames({
-                  "text-zinc-900": link.href === currentPath,
-                  "text-zinc-500": link.href !== currentPath,
-                  "hover:text-zinc-800 transition-colors": true,
-                })}
-              >
-                {link.label}
-              </Link>
-            ))}
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end space-x-10">
-        <div className="flex cursor-pointer gap-2">
-          <LuSun size={20} />
-          <input
-            type="checkbox"
-            value="dark"
-            className="toggle theme-controller"
-          />
-          <LuMoon size={20} />
+        <div className="navbar-end">
+          <Link href="#" className="btn">
+            Button
+          </Link>
         </div>
-        <Link href="#" className="btn">
-          Button
-        </Link>
       </div>
     </nav>
   );
