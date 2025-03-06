ea4c70f548f0c61995c0f3728233b73cc44d09dc
GovanDBT <govan.dibotelo@gmail.com>
2025-03-07 00:26:41 +0200

    make login form

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div className="bg-neutral p-10 rounded-lg flex-row">
+      <h1 className="text-white text-4xl mb-2">Login to your Project</h1>
+      <p className="text-white mb-3">
+        Enter your credentials belows to manage your final year project with
+        ease
+      </p>
+      <fieldset className="fieldset mb-5">
+        <legend className="fieldset-legend text-white text-base">
+          Your Email or ID
+        </legend>
+        <label className="input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input type="email" placeholder="mail@site.com" required />
+        </label>
+        <div className="validator-hint hidden">Enter valid email address</div>
+        <legend className="fieldset-legend text-white text-base">
+          Your Password
+        </legend>
+        <label className="input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+        </label>
+        <p className="validator-hint hidden">
+          Must be more than 8 characters, including
+          <br />
+          At least one number
+          <br />
+          At least one lowercase letter
+          <br />
+          At least one uppercase letter
+        </p>
+      </fieldset>
+      <button className="btn btn-primary">Login</button>
+    </div>
+  );
+};
+
+export default LoginForm;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import NavBar from "./components/NavBar";
+import LoginForm from "./components/LoginForm";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <div className="grid grid-rows-2 grid-cols-1 lg:grid-cols-2 lg:grid-rows-1 container mx-auto">
-        <h1>Login</h1>
+      <div className="grid grid-rows-2 gap-5 grid-cols-1 lg:grid-cols-[1fr_2fr] lg:grid-rows-1 container mx-auto">
+        <LoginForm />
         <h1>Main</h1>
       </div>
     </div>
