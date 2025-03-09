37eafb15f0102438c07e3e27a1e2c649de849304
GovanDBT <govan.dibotelo@gmail.com>
2025-03-09 09:32:21 +0200

    building a dummy api

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  params: { id: number };
+}
+
+export function GET(request: NextRequest, { params }: Props) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: "Thembi" });
+}
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(request: NextRequest, { params }: Props) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({});
+}
--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,20 @@ export default function Home() {
       <NavBar />
       <div className="grid grid-rows-2 gap-5 grid-cols-1 lg:grid-cols-[1fr_2fr] lg:grid-rows-1 container mx-auto">
         <LoginForm />
-        <div className="flex items-start gap-5">
-          <SubmissionCountdown
-            title="Days till next submission"
-            days={11}
-            desc="Project proposal - documentation of introduction, literature review, and system analysis"
-          />
-          <SubmissionCountdown
-            title="Days final year presentation"
-            days={45}
-            desc="Presentation of project - including presentation slides, documentation, and implementation"
-          />
+        <div>
+          <div className="flex items-start gap-5 mb-8">
+            <SubmissionCountdown
+              title="Days till next submission"
+              days={11}
+              desc="Project proposal - documentation of introduction, literature review, and system analysis"
+            />
+            <SubmissionCountdown
+              title="Days final year presentation"
+              days={45}
+              desc="Presentation of project - including presentation slides, documentation, and implementation"
+            />
+          </div>
+          <h1 className="text-3xl">Announcements</h1>
         </div>
       </div>
     </div>
