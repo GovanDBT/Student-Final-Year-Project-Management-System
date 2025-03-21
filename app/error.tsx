21b87978fe4f4c45c3f10845e076e0ceaa74595b
GovanDBT <govan.dibotelo@gmail.com>
2025-03-21 12:29:38 +0200

    implementing error handling

---
+++ app/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import { FaLinkSlash } from "react-icons/fa6";
+
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+
+const ErrorPage = ({ error, reset }: Props) => {
+  console.log("Error", error);
+  return (
+    <div className="flex-col space-y-5 h-lvh place-content-center place-items-center">
+      <div className="flex gap-3 items-center">
+        <FaLinkSlash size={20} />
+        <h3>An unexpected error has occurred</h3>
+      </div>
+      <button className="btn btn-primary" onClick={() => reset()}>
+        Refresh
+      </button>
+    </div>
+  );
+};
+
+export default ErrorPage;
