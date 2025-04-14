cf8e04a9f845176d32558d2be25fc99cc3d609ed
GovanDBT <govan.dibotelo@gmail.com>
2025-04-14 10:29:20 +0200

    customize student dashboard

--- app/dashboard/student/page.tsx
+++ app/dashboard/student/page.tsx
@@ -1,18 +1,37 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
-import Link from "next/link";
 import { prisma } from "@/prisma/client";
+import classnames from "classnames";
 
 import SubmissionCountdown from "@/app/components/SubmissionCountdown";
+import Announcement from "@/app/components/Announcement";
+import Link from "next/link";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+  const currentUserId = await prisma.user.findUnique({
+    where: { email: session?.user?.email ?? undefined },
+    select: { userId: true },
+  });
+
+  const project = await prisma.project.findUnique({
+    where: { studentId: currentUserId?.userId ?? undefined },
+    include: {
+      supervisor: {
+        select: {
+          name: true,
+        },
+      },
+      student: true,
+    },
+  });
+
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl">
         👋 Hello {session && <span>{session.user!.name}</span>}
       </h1>
-      <div className="flex items-start gap-2 lg:gap-5 my-5">
+      <div className="md:flex items-start gap-2 lg:gap-5 my-5">
         <SubmissionCountdown
           title="Days till next submission"
           days={11}
@@ -29,11 +48,82 @@ export default async function Dashboard() {
           desc="Presentation of project - including presentation slides, documentation, and implementation"
         />
       </div>
-      <div className="grid grid-cols-3 my-5">
-        <div className="col-span-2">Project Details</div>
-        <div>calendar</div>
-        <div className="col-span-2">Announcements</div>
-        <div>Activities</div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 my-5">
+        {/** Project Overview */}
+        <div className="col-span-2">
+          <div className="divider divider-start mb-8">
+            <h2>Project Overview</h2>
+          </div>
+          {!project?.studentId && (
+            <div className="h-50 bg-base-200 rounded-lg p-5 place-items-center content-center space-y-5">
+              <h1>You Do Not Have A Project Proposal!</h1>
+              <button className="btn btn-primary">
+                <Link href="/dashboard/student/project/proposal">
+                  Create Proposal
+                </Link>
+              </button>
+            </div>
+          )}
+          {project?.studentId && (
+            <div className="grid md:grid-cols-2 gap-5">
+              <div className="space-y-5">
+                <p>
+                  <span className="font-bold">Project Title:</span>{" "}
+                  {project?.title}
+                </p>
+                <p>
+                  <span className="font-bold">Project Supervisor:</span>{" "}
+                  {project?.supervisor.name}
+                </p>
+              </div>
+              <div className="space-y-4">
+                <div className="flex gap-10 mt-2 mb-5">
+                  <p>
+                    <span className="font-bold">Project Status:</span>{" "}
+                    <span
+                      className={classnames({
+                        "badge-warning": project?.status === "PENDING",
+                        "badge-success": project?.status === "APPROVED",
+                        "badge-error": project?.status === "REJECTED",
+                        "badge-info": project?.status === "COMPLETED",
+                        "badge badge-soft": true,
+                      })}
+                    >
+                      {project?.status}
+                    </span>
+                  </p>
+                </div>
+                <p>
+                  <span className="font-bold">Last Update:</span>{" "}
+                  {project?.dateUpdated?.toLocaleDateString()}
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+        {/** Calendar */}
+        <div>
+          <div className="divider divider-start mb-8">
+            <h2>Calendar</h2>
+          </div>
+        </div>
+        {/** Announcements */}
+        <div className="col-span-2">
+          <div className="divider divider-start mb-8">
+            <h2>Announcements</h2>
+          </div>
+          <div className="max-h-105 overflow-scroll p-3 border-1 border-secondary/10 rounded-lg">
+            <Announcement />
+            <Announcement />
+            <Announcement />
+          </div>
+        </div>
+        {/** Activities */}
+        <div>
+          <div className="divider divider-start mb-8">
+            <h2>Activities</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
